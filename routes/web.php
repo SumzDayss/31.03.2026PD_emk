6c4fff427612ae5f1eb1231c7dbf60981ddd63fb
elinaviksne <elina@gmail.com>
2026-03-31 13:48:07 +0300

    Started doing edit form,not finished yet

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,6 @@ Route::get('/books', [BookController::class, 'index']);
 Route::get('/books/create', [BookController::class, 'update']);
 Route::post('/books', [BookController::class, 'store']);
 Route::get('/books/{id}', [BookController::class, 'show']);
+Route::get('/books/{id}', [BookController::class, 'create']);
 Route::get('/books/{id}/edit', [BookController::class, 'edit']);
 Route::delete('/books/{id}/destroy', [BookController::class, 'destroy']);
